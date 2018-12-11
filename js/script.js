09cc2adf0f7c894fef06c5100c3d3a55858a70b5
YaroslavHumeniuk <yaroslavhumeniuk@gmail.com>
2018-12-11 03:06:36 +0100

    +JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-c
+var numbers = document.querySelectorAll('.calculator__button');
+    console.log(numbers);
+    operations = document.querySelectorAll('.operation'),
+    decimalBtn = document.querySelector('.decimals'),
+    clearBtn = document.querySelector('clear'),
+    resultBtn = document.querySelector('result');
+
+
+
+function numberPress () {
+	 
+};
+
+function operation () {
+	 
+};
+
+function decimal () {
+	 
+};
+
+function clear () {
+	 
+};
+
